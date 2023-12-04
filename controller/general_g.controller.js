b505c62828a301e1fbe70285ede0db0cb3825b4d
alicevaliha <alicevaliha@gmail.com>
2023-12-04 07:11:21 +0300

    group by

--- controller/general_g.controller.js
+++ controller/general_g.controller.js
@@ -2,7 +2,7 @@ const pool = require("../database/index")
 const general_gController = {
     getAll: async (req, res) => {
         try {
-            const [rows, fields] = await pool.query("select * from v_GenAll_stats")
+            const [rows, fields] = await pool.query("select * from v_GenAll_stats group by equipe , competition_buts ")
             res.json({
                 data: rows
             })
@@ -16,7 +16,7 @@ const general_gController = {
     getById: async (req, res) => {
         try {
             const { id } = req.params
-            const [rows, fields] = await pool.query("select * from v_GenAll_stats where id_equipe = ?", [id])
+            const [rows, fields] = await pool.query("select * from v_GenAll_stats where id_equipe = ? group by equipe , competition_buts ", [id])
             res.json({
                 data: rows
             })
